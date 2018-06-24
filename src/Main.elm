2cc80aa5bf9a8a3d238686cbd745e8343c5b4405
Parijat Mishra <parijat.mishra@gmail.com>
2018-06-24 14:25:41 +0800

    Handle add todo, toggle done, delete

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (Html)
-import Html.Attributes as Attr
-
+import Html exposing (Html, div, span, input, text, button, i)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (class, classList, placeholder, type_, checked, disabled, value)
 
 main : Program Never Model Msg
 main =
@@ -20,11 +20,14 @@ main =
 
 type alias Model =
     { todos : List Todo
+    , newTodoText: String
+    , nextId: Int
     }
 
 
 type alias Todo =
-    { title : String
+    { id: Int
+    , title : String
     , complete : Bool
     }
 
@@ -37,16 +40,21 @@ init =
 initModel : Model
 initModel =
     { todos =
-        [ { title = "Add checkbox"
+        [ { id = 1
+          , title = "Add checkbox"
           , complete = True
           }
-        , { title = "Add form"
+        , { id = 2
+          , title = "Add form"
           , complete = True
           }
-        , { title = "Handle Submit"
+        , { id = 3
+          , title = "Handle Submit"
           , complete = False
           }
         ]
+    , newTodoText = ""
+    , nextId = 4
     }
 
 
@@ -54,21 +62,39 @@ initModel =
 --- MESSAGES
 
 
-type Msg
-    = NoOp
-
-
+type Msg =
+    ChangeText String
+    | AddTodo
+    | ToggleTodo Int
+    | DeleteTodo Int
 
 --- UPDATE
-
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
+        ChangeText newText ->
+            ( {model | newTodoText = String.trim newText}, Cmd.none )
+        AddTodo ->
+            let newTodo = { id = model.nextId, title = model.newTodoText, complete = False }
+                todos = newTodo :: model.todos
+                nextId = model.nextId + 1
+            in
+                ( { model | todos = todos, newTodoText = "", nextId = nextId }, Cmd.none )
+        ToggleTodo id ->
+            let
+                f todo =
+                    if id == todo.id
+                    then { todo | complete = not todo.complete }
+                    else todo
+                todos = List.map f model.todos
+            in
+                ({ model | todos = todos }, Cmd.none )
+        DeleteTodo id ->
+            let
+                todos = List.filter (\t -> t.id /= id) model.todos
+            in
+                ({ model | todos = todos }, Cmd.none )
 
 --- VIEW
 
@@ -77,19 +103,49 @@ view : Model -> Html Msg
 view model =
     Html.div []
         [ Html.h1 [] [ Html.h1 [] [ Html.text "Todos" ] ]
-        , Html.ul [] (List.map viewTodo model.todos)
-        , Html.h2 [] [ Html.text "Add new" ]
-        , Html.input [] []
-        , Html.button [] [ Html.text "Add" ]
+        , viewNewTodoForm model
+        , viewTodoList model
         ]
 
+viewTodoList : Model -> Html Msg
+viewTodoList model =
+    div [ class "p1" ] (List.map viewTodoItem model.todos)
 
-viewTodo : Todo -> Html Msg
-viewTodo todo =
-    Html.li [] [ Html.input [ Attr.type_ "checkbox", Attr.checked todo.complete ] []
-               , Html.text todo.title ]
+viewTodoItem : Todo -> Html Msg
+viewTodoItem todo =
+    div [ class "clearfix p1"]
+        [ div   [ class "col col-10" ]
+                [ span  [ class <| todoItemBassClass todo ]
+                        [ text todo.title ]
+                ]
+        , div   [ class "col ml2" ]
+                [ input [ type_ "checkbox", checked todo.complete, onClick (ToggleTodo todo.id) ]
+                        []
+                ]
+        , div   [ class "col ml2" ]
+                [ button    [ onClick (DeleteTodo todo.id) ]
+                            [ text "Delete" ]
+                ]
+        ]
 
+todoItemBassClass : Todo -> String
+todoItemBassClass todo =
+    if todo.complete
+    then "underline italic"
+    else "bold"
 
+viewNewTodoForm : Model -> Html Msg
+viewNewTodoForm model =
+        div [ class "p1" ]
+            [ div   []
+                    [ input [ placeholder "Add new todo", onInput ChangeText, value model.newTodoText ]
+                            []
+                    ]
+            , div   []
+                    [ button    [ onClick AddTodo, disabled (String.isEmpty model.newTodoText) ]
+                                [ text "Add" ]
+                    ]
+            ]
 
 --- SUBSCRIPTIONS
 
