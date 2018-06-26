db201d719cda5143a3a76af36a86c46448e64521
Parijat Mishra <parijat.mishra@gmail.com>
2018-06-26 22:34:37 +0800

    Add delay in dummyserver to see effect on UI

--- dummyserver/api.js
+++ dummyserver/api.js
@@ -1,6 +1,11 @@
 var jsonServer = require('json-server');
 var server = jsonServer.create();
 server.use(jsonServer.defaults());
+
+// Introduce a delay -- if you want to see how the UI will behave when net is slow
+const pause = require('connect-pause');
+server.use(pause(1000));
+
 var router = jsonServer.router('db.json');
 server.use('/api', router);
 
